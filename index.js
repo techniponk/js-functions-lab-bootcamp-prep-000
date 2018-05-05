b2d66c143a36355530fb917f69718717c0fd72ed
Nikki M <nrmonegain@gmail.com>
2018-05-05 19:18:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays() {
 }
 
 function happyHolidaysTo(name) {
-  return console.log ("Happy holidays, ${name}!")
+  return "Happy holidays, ${name}!"
 }
 
 function happyHolidayTo(holiday, name) {
